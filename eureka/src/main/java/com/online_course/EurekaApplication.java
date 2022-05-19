bf4f2787ce326bf342031418c6cadb8665f9d580
wenhaoli <m18720955369@163.com>
2022-05-19 19:01:57 +0100

    1.增加eureka子模块作为注册服务端 2.将单maven项目改造为maven父子项目

--- eureka/src/main/java/com/online_course/EurekaApplication.java
+++ eureka/src/main/java/com/online_course/EurekaApplication.java
@@ -1,13 +1,13 @@
-package com.learn_forever.learn_forever;
+package com.online_course;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LearnForeverApplication {
+public class EurekaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LearnForeverApplication.class, args);
+		SpringApplication.run(EurekaApplication.class, args);
 	}
 
 }
