2080d68176079e36dfc0dd8c0bc09d9ed4666085
wenhaoli <m18720955369@163.com>
2022-05-19 18:51:12 +0100

    1.spring项目初始提交

---
+++ src/main/java/com/learn_forever/learn_forever/LearnForeverApplication.java
@@ -0,0 +1,13 @@
+package com.learn_forever.learn_forever;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnForeverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnForeverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learn_forever/learn_forever/LearnForeverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn_forever.learn_forever;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnForeverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
