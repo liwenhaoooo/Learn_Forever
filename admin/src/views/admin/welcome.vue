acfc549cb5df40d8d8762f41c92cb581abff4863
wenhaoli <m18720955369@163.com>
2022-06-15 19:26:12 +0100

    1.z增加了business模块，并增加初始启动代码

---
+++ admin/src/views/admin/welcome.vue
@@ -0,0 +1,7 @@
+<template>
+<div>
+  <h1>
+    您好，欢迎进入后台系统！
+  </h1>
+</div>
+</template>
---
+++ business/src/main/java/com/onlien_course/business/controller/TestController.java
@@ -0,0 +1,23 @@
+package com.onlien_course.business.controller;
+
+import com.online_course.server.domain.Test;
+import com.online_course.server.service.TestService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * @author wenhaoli
+ * @date 2022/05/19 19:17
+ */
+@RestController
+public class TestController {
+    @Resource
+    private TestService testService;
+    @RequestMapping("/test")
+    public List<Test> test(){
+        return testService.list();
+    }
+}
