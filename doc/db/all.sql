f5790369e0490f1433a70df31a6c6c0d0132fe30
wenhaoli <m18720955369@163.com>
2022-06-15 21:33:57 +0100

    1.增加了Chapter表sql，生成持久层代码

--- doc/db/all.sql
+++ doc/db/all.sql
@@ -1,3 +1,12 @@
+drop table if exists `chapter`;
+create table `chapter` (
+   `id` char(8) not null comment 'ID',
+   `course_id` char(8) comment 'CourseID',
+   `name` varchar(50) comment 'Name',
+   primary key (`id`)
+) engine=innodb default charset=utf8mb4 comment='Chapter';
+
+
 # ---------------------- 测试
 
 drop table if exists `test`;
