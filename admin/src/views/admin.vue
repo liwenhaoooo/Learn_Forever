6a94db08e2474e26d2c569c65c1ed0111d17561d
wenhaoli <m18720955369@163.com>
2022-06-16 12:40:50 +0100

    1.新增chapter页面及子路由，并使用假数据制作表格

--- admin/src/views/admin.vue
+++ admin/src/views/admin.vue
@@ -252,24 +252,27 @@
     <div class="content-wrapper">
       <!-- Content Header (Page header) -->
       <section class="content-header">
-        <div class="container-fluid">
-          <div class="row mb-2">
-            <div class="col-sm-6">
-              <h1>Welcome Page</h1>
-            </div>
+<!--        <div class="container-fluid">-->
+<!--          <div class="row mb-2">-->
+<!--            <div class="col-sm-6">-->
+<!--            </div>-->
 <!--            <div class="col-sm-6">-->
 <!--              <ol class="breadcrumb float-sm-right">-->
 <!--                <li class="breadcrumb-item"><a href="#">Home</a></li>-->
 <!--                <li class="breadcrumb-item active">Blank Page</li>-->
 <!--              </ol>-->
 <!--            </div>-->
-          </div>
-        </div><!-- /.container-fluid -->
+<!--          </div>-->
+<!--        </div>&lt;!&ndash; /.container-fluid &ndash;&gt;-->
       </section>
 
       <!-- Main content -->
       <section class="content">
+        <div class="row">
+          <div class="col-12">
       <router-view/>
+          </div>
+        </div>
       </section>
       <!-- /.content -->
     </div>
