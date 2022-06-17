f09bf2058811ee7862f413d953fa888999a60da1
wenhaoli <m18720955369@163.com>
2022-06-17 21:52:05 +0100

    1. 增加工具类tool.js和校验validator.js 2. 大章保存非空和长度增加校验

---
+++ admin/public/static/js/validator.js
@@ -0,0 +1,19 @@
+Validator = {
+  require: function (value, text) {
+    if (Tool.isEmpty(value)) {
+      Toast.warning(text +" "+ "is required !");
+      return false;
+    } else {
+      return true
+    }
+  },
+
+  length: function (value, text, min, max) {
+    if (!Tool.isLength(value, min, max)) {
+      Toast.warning(text +" "+ "Maximum" +" " + min + "~" + max +" " + "characters");
+      return false;
+    } else {
+      return true
+    }
+  }
+};
--- admin/src/views/admin/chapter.vue
+++ admin/src/views/admin/chapter.vue
@@ -143,6 +143,14 @@ export default {
 
     save(page) {
       let _this = this;
+
+      // 保存校验
+      if (!Validator.require(_this.chapter.name, "Name")
+          || !Validator.require(_this.chapter.courseId, "CourseID")
+          || !Validator.length(_this.chapter.courseId, "CourseID", 1, 8)) {
+        return;
+      }
+
       Loading.show();
       _this.$ajax.post('http://127.0.0.1:10000/business/admin/chapter/save', _this.chapter).then((response)=>{
         Loading.hide();
