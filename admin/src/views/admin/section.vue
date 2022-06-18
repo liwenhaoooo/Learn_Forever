a4edf5ce6a2d5d2b9727a9f0961726a3f9cb45b7
wenhaoli <m18720955369@163.com>
2022-06-18 21:28:43 +0100

    1. 增加枚举常量

--- admin/src/views/admin/section.vue
+++ admin/src/views/admin/section.vue
@@ -130,7 +130,7 @@ export default {
     return {
       section: {},
       sections: [],
-      CHARGE: [{key:"C", value:"Charge"},{key:"F", value:"Free"}],
+      CHARGE: CHARGE,
     }
   },
   mounted: function() {
